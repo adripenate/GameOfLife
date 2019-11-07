73a88ba7bd1f4b8dc176bb8f5a05f843d5488185
adripsau <adrianapenate98@gmail.com>
2019-11-07 15:16:58 +0000

    Red test - not_kill_or_create_cells

--- GameOfLife.Tests/GameShould.cs
+++ GameOfLife.Tests/GameShould.cs
@@ -7,9 +7,36 @@ namespace GameOfLife.Tests
     public class GameShould
     {
         [Test]
-        public void test()
+        public void not_kill_or_create_cells()
         {
-            
+            var initialWorld = new World(4, 8);
+            var outputWorld = new Game().calculateNextGeneration(initialWorld);
+            outputWorld.LiveCells.Count.Should().Be(0);
         }
+    }
 
+    public class Game
+    {
+        public World calculateNextGeneration(World initialWorld)
+        {
+            return null;
+        }
+    }
+
+    public class World
+    {
+        private int Rows { get; set; }
+        private int Columns { get; set; }
+        public List<Cell> LiveCells { get; private set; }
+
+        public World(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+    }
+
+    public class Cell
+    {
+    }
 }
